ff60ce275b9d14a48be4c94ff4ae2255ab72b0ee
Makise <844082183@qq.com>
2019-05-03 21:56:06 +0800

    修正重复打印日志的bug

--- src/main/java/webby/config/WebbyLogConfig.java
+++ src/main/java/webby/config/WebbyLogConfig.java
@@ -24,8 +24,7 @@ import java.util.Properties;
  **/
 public class WebbyLogConfig {
     private static LogConfig config;
-    private static Date date = new Date();
-    private static SimpleDateFormat spf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+    private static String date = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
 
     /**
      * Description: 读取配置，并给logback进行配置
@@ -39,7 +38,7 @@ public class WebbyLogConfig {
         //从配置文件中读取配置
         config.setLevel(String.valueOf(properties.getOrDefault("webby.log.level", "INFO")));
         config.setConsolePattern(String.valueOf(properties.getOrDefault("webby.log.append.console.pattern", "%d{yyyy/MM/dd HH:mm:ss.SSS} [%thread] [%X{requestId}] %-5level %logger{36} - %msg%n")));
-        config.setFileName(String.valueOf(properties.getOrDefault("webby.log.append.file.name", "./logs/{app.name}_{date}.log")));
+        config.setFileName(String.valueOf(properties.get("webby.log.append.file.name")));
         config.setFilePattern(String.valueOf(properties.getOrDefault("webby.log.append.file.pattern", "%d{yyyy/MM/dd HH:mm:ss.SSS} [%thread] [%X{requestId}] %-5level %logger{36} - %msg%n")));
 
         startLog();
@@ -49,6 +48,8 @@ public class WebbyLogConfig {
         //设置输出等级
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
         Logger logger = lc.getLogger("webby");
+        logger.setLevel(Level.toLevel(config.getLevel()));
+        lc.getLogger("io.netty").setLevel(Level.toLevel(config.getLevel()));
 
         //设置输出在控制台的格式
         PatternLayoutEncoder encoder = new PatternLayoutEncoder();
@@ -57,20 +58,17 @@ public class WebbyLogConfig {
         encoder.setPattern(formart(config.getConsolePattern()));
         encoder.setImmediateFlush(true);
 
-        ConsoleAppender<ILoggingEvent> ca = new ConsoleAppender<ILoggingEvent>();
+        Logger rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME);
+        ConsoleAppender<ILoggingEvent> ca = (ConsoleAppender<ILoggingEvent>) rootLogger.getAppender("console");
         ca.setContext(lc);
-        ca.setName("console");
         ca.setEncoder(encoder);
 
         encoder.start();
         ca.start();
-        logger.addAppender(ca);
-
+        rootLogger.addAppender(ca);
 
         //如果要在文件中输出，就设置输出器
-        if (StringUtils.isBlank(config.getFileName())){
-            return;
-        }else {
+        if (!StringUtils.isBlank(config.getFileName()) && !"null".equals(config.getFileName())){
             PatternLayoutEncoder fileencoder = new PatternLayoutEncoder();
             fileencoder.setContext(lc);
             fileencoder.setCharset(Charset.forName("UTF-8"));
@@ -88,13 +86,11 @@ public class WebbyLogConfig {
             fa.start();
             logger.addAppender(fa);
         }
-        logger.setLevel(Level.toLevel(config.getLevel()));
-        lc.getLogger("io.netty").setLevel(Level.toLevel(config.getLevel()));
     }
 
     private static String formart(String s){
         s = s.replace("{app.name}", WebbyConfig.getConfig().getAppName());
-        s = s.replace("{date}", spf.format(date));
+        s = s.replace("{date}", date);
         return s;
     }
 
